0446b92e37801d95c8e725335575b013c51b42f4
Lars Tesmer <lars.tesmer@gmail.com>
2013-12-17 22:42:11 +0100

    Fix SQL injection

--- events/participe.php
+++ events/participe.php
@@ -5,17 +5,19 @@ include("../scripts/identifiants.php");
          
      if($_GET['part']=="Participer")
 	 {
-	 mysql_query("insert into eventuser (iduser,idevent) values('".$_GET['user']."','".$_GET['event']."')")or die(mysql_error());
-	 mysql_query("update event set nbpart = nbpart+1 where id='".$_GET['event']."'")or die(mysql_error());
+	 $user = mysql_real_escape_string($_GET['user']);
+	 $event = mysql_real_escape_string($_GET['event']);
+
+	 mysql_query("insert into eventuser (iduser,idevent) values('".$user."','".$event."')")or die(mysql_error());
+	 mysql_query("update event set nbpart = nbpart+1 where id='".$event."'")or die(mysql_error());
 	 }
 	 if($_GET['part']=="Annuler")
 	 {
-		mysql_query("delete from eventuser where idevent='".$_GET['event']."' and iduser = '".$_GET['user']."' ")or die(mysql_error());
-		mysql_query("update event set nbpart = nbpart-1 where id='".$_GET['event']."'")or die(mysql_error());
+		mysql_query("delete from eventuser where idevent='".$event."' and iduser = '".$user."' ")or die(mysql_error());
+		mysql_query("update event set nbpart = nbpart-1 where id='".$event."'")or die(mysql_error());
 	 }
 	 if($_GET['part']=="Supprimer")
 	 {
-		mysql_query("delete from event where id='".$_GET['event']."' ")or die(mysql_error()); 
+		mysql_query("delete from event where id='".$event."' ")or die(mysql_error());
 	 }
     ?>
-	
